468de1ba25cf76fd4aaecd99ca195b204087b79f
Zeeshan Shabir <149652993+zeeshanshabir07@users.noreply.github.com>
2024-08-21 20:19:30 -0700

    Initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+function Navigation(){
+    return(
+        <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li>MENU</li>
+          <li>LOCATION</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+        <button>login</button>
+      </nav>
+    );
+};
+ export default Navigation;
